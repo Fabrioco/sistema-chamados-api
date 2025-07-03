90facba46a95195bf7a4e59bacd1850837632112
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-07-03 15:27:55 -0300

    feat(user-repository-auth): adicionei as funçẽso de criar token, comparar senhas e codificar senhas

--- src/modules/auth/user.repository.ts
+++ src/modules/auth/user.repository.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { RegisterDTO } from "./dtos/register.dto";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -13,6 +15,20 @@ export class UserRepository {
   async create(data: RegisterDTO) {
     return await prisma.user.create({
       data,
+    });
+  }
+
+  async hashPassword(password: string) {
+    return await bcrypt.hash(password, 10);
+  }
+
+  async comparePassword(password: string, hash: string) {
+    return await bcrypt.compare(password, hash);
+  }
+
+  createToken(email: string) {
+    return jwt.sign({ email }, process.env.JWT_SECRET as string, {
+      expiresIn: "1d",
     });
   }
 }
--- src/utils/compare-password.ts
+++
@@ -1,5 +0,0 @@
-import bcrypt from "bcrypt";
-
-export async function comparePassword(password: string, hash: string) {
-  return await bcrypt.compare(password, hash);
-}
