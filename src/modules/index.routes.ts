f934d6b1c2f3f7f1e40fda8eeeb98851b0b4420b
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-07-05 17:49:43 -0300

    feat(index-routes): adicionei a rota de tickets

--- src/modules/index.routes.ts
+++ src/modules/index.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import AuthUserRoutes from "./auth/auth.routes";
+import TicketRoutes from "./tickets/ticket.routes";
 
 const routes = Router();
 
 routes.use("/auth", AuthUserRoutes);
+routes.use("/tickets", TicketRoutes);
 
 export default routes;
