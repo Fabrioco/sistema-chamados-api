a0cf72c7328ce4e6071dc9a0269af94a1cb9ae82
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-07-07 01:09:09 -0300

    feat(server): separei do app para apenas foco no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 import routes from "./modules/index.routes";
 
 dotenv.config();
 
 const app = express();
-const prisma = new PrismaClient();
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
