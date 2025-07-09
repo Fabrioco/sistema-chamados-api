1348446a963470ac5bc5a9cb76fea07a85bbd4af
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-07-08 23:34:45 -0300

    feat(app): adicionei o uso do cookie-parser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import routes from "./modules/index.routes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(cookieParser());
 
 export default app;
